2df1587f0945501ff2a4942c29a58d69f3325cd0
Don Athalage <dknathalage@gmail.com>
2022-06-07 19:51:30 +1000

    climbing-stairs

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -7,6 +7,19 @@ def climbStairs(n):
     :type n: int
     :rtype: int
     """
+    if n < 4:
+        return n
 
-    
-    
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3,n+1):
+        dp[i] = sum(dp[i-2:i])
+
+    return dp[-1]
+
+
+out = climbStairs(10)
+print(out)
